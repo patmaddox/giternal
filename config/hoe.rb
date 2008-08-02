53e53bf45b887eb1d7abcb31c4a799269820d982
Pat Maddox <pat.maddox@gmail.com>
2008-08-02 02:38:10 -0400

    updated manifest for gem

--- config/hoe.rb
+++ config/hoe.rb
@@ -56,7 +56,7 @@ $hoe = Hoe.new(GEM_NAME, VERS) do |p|
   p.summary = DESCRIPTION
   p.url = HOMEPATH
   p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
-  p.test_globs = ["test/**/test_*.rb"]
+  p.test_globs = ["spec/**/*_spec.rb"]
   p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
 
   # == Optional
