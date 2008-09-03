d9830ba89bc556df09cc29632023ddacd83c364f
Pat Maddox <pat.maddox@gmail.com>
2008-09-03 19:33:27 -0400

    wrote first cucumber story

---
+++ features/checking_out_externals.feature
@@ -0,0 +1,10 @@
+Feature: Checking out and updating externals
+  As a developer
+  I want to check out and update external projects via git
+  So that I can add functionality to my app with little effort
+
+  Scenario: Repository is not yet checked out
+    Given an external repository named 'first_external'
+    And 'first_external' is not yet checked out
+    When I update the externals
+    Then 'first_external' should be checked out
---
+++ features/steps/repository_steps.rb
@@ -0,0 +1,106 @@
+require 'spec'
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'giternal'
+
+class GiternalHelper
+  @@tmp_path = File.expand_path(File.dirname(__FILE__) + '/../tmp')
+  
+  def self.create_main_repo
+    FileUtils.mkdir_p tmp_path
+    Dir.chdir(tmp_path) do
+      FileUtils.mkdir "main_repo"
+    end
+  end
+
+  def self.tmp_path
+    @@tmp_path
+  end
+
+  def self.create_repo(repo_name)
+    Dir.chdir(tmp_path) do
+      FileUtils.mkdir_p "externals/#{repo_name}"
+      `cd externals/#{repo_name} && git init`
+    end
+    add_to_config_file repo_name
+  end
+
+  def self.add_to_config_file(repo_name)
+    config_dir = tmp_path + '/main_repo/config'
+    FileUtils.mkdir(config_dir) unless File.directory?(config_dir)
+    Dir.chdir(config_dir) do
+      File.open("giternal.yml", "w") do |file|
+        file << "#{repo_name}:\n"
+        file << "  repo: #{external_path(repo_name)}\n"
+        file << "  path: dependencies\n"
+      end
+    end
+  end
+
+  def self.add_content(repo_name)
+    Dir.chdir(tmp_path + "/externals/#{repo_name}") do
+      `echo foo >> foo && git add foo`
+      `git commit foo -m "added content to foo"`
+    end
+  end
+
+  def self.external_path(repo_name)
+    File.expand_path(tmp_path + "/externals/#{repo_name}")
+  end
+
+  def self.checked_out_path(repo_name)
+    File.expand_path(tmp_path + "/main_repo/dependencies/#{repo_name}")
+  end
+
+  def self.clean!
+    FileUtils.rm_rf tmp_path
+  end
+
+  def self.update_externals
+    Dir.chdir(tmp_path + '/main_repo') do
+      `rake -f #{sakefile_path} giternal:update`
+    end
+  end
+
+  def self.sakefile_path
+    File.expand_path(tmp_path + '/../../lib/tasks/giternal.rake')
+  end
+
+  def self.repo_contents(path)
+    Dir.chdir(path) do
+      contents = `git cat-file -p HEAD`
+      unless contents.include?('tree') && contents.include?('author')
+        raise "something is wrong with the repo, output doesn't contain expected git elements:\n\n #{contents}"
+      end
+      contents
+    end
+  end
+end
+
+Before do
+  GiternalHelper.clean!
+  GiternalHelper.create_main_repo
+end
+
+After do
+#  GiternalHelper.clean!
+end
+
+Given /an external repository named '(.*)'/ do |repo_name|
+  GiternalHelper.create_repo repo_name
+  GiternalHelper.add_content repo_name
+end
+
+Given /'(.*)' is not yet checked out/ do |repo_name|
+  # TODO: Figure out why I can't use should be_false here
+  File.directory?(GiternalHelper.checked_out_path(repo_name)).should == false
+end
+
+When "I update the externals" do
+  GiternalHelper.update_externals
+end
+
+Then /'(.*)' should be checked out/ do |repo_name|
+  File.directory?(GiternalHelper.checked_out_path(repo_name)).should == true
+  GiternalHelper.repo_contents(GiternalHelper.checked_out_path(repo_name)).should ==
+    GiternalHelper.repo_contents(GiternalHelper.external_path(repo_name))
+end
